6ff8f9c4709a519b1f98a98b510f4b041d0b5764
libinyl <libin152@outlook.com>
2020-09-07 10:03:38 +0800

    update

--- include/ip.h
+++ include/ip.h
@@ -1,9 +1,9 @@
 #ifndef IPV4_H
 #define IPV4_H
-#include "syshead.h"
 #include "ethernet.h"
 #include "skbuff.h"
 #include "sock.h"
+#include "syshead.h"
 
 #define IPV4 0x04
 #define IP_TCP 0x06
@@ -13,25 +13,24 @@
 #define ip_len(ip) (ip->len - (ip->ihl * 4))
 
 #ifdef DEBUG_IP
-#define ip_dbg(msg, hdr)                                                \
-    do {                                                                \
-        print_debug("ip "msg" (ihl: %hhu version: %hhu tos: %hhu "   \
-                    "len %hu id: %hu frag_offset: %hu ttl: %hhu " \
-                    "proto: %hhu csum: %hx " \
-                    "saddr: %hhu.%hhu.%hhu.%hhu daddr: %hhu.%hhu.%hhu.%hhu)", \
-                    hdr->ihl,                                           \
-                    hdr->version, hdr->tos, hdr->len, hdr->id,          \
-                    hdr->frag_offset, hdr->ttl, hdr->proto, hdr->csum,   \
-                    hdr->saddr >> 24, hdr->saddr >> 16, hdr->saddr >> 8, hdr->saddr >> 0, \
-                    hdr->daddr >> 24, hdr->daddr >> 16, hdr->daddr >> 8, hdr->daddr >> 0); \
+#define ip_dbg(msg, hdr)                                                                                               \
+    do {                                                                                                               \
+        print_debug("ip " msg " (ihl: %hhu version: %hhu tos: %hhu "                                                   \
+                    "len %hu id: %hu frag_offset: %hu ttl: %hhu "                                                      \
+                    "proto: %hhu csum: %hx "                                                                           \
+                    "saddr: %hhu.%hhu.%hhu.%hhu daddr: %hhu.%hhu.%hhu.%hhu)",                                          \
+                    hdr->ihl, hdr->version, hdr->tos, hdr->len, hdr->id, hdr->frag_offset, hdr->ttl, hdr->proto,       \
+                    hdr->csum, hdr->saddr >> 24, hdr->saddr >> 16, hdr->saddr >> 8, hdr->saddr >> 0, hdr->daddr >> 24, \
+                    hdr->daddr >> 16, hdr->daddr >> 8, hdr->daddr >> 0);                                               \
     } while (0)
 #else
 #define ip_dbg(msg, hdr)
 #endif
 
 struct iphdr {
-    uint8_t ihl : 4; /* TODO: Support Big Endian hosts */
-    uint8_t version : 4;
+    uint8_t ihl : 4; /* TODO: Support Big Endian hosts */ // ihl = Internet Header Length,  IPv4 header 长度 , >=5 ( 20
+                                                          // 字节 ) && <= 15 (60 字节)
+    uint8_t version : 4; // 版本
     uint8_t tos;
     uint16_t len;
     uint16_t id;
@@ -44,15 +43,11 @@ struct iphdr {
     uint8_t data[];
 } __attribute__((packed));
 
-static inline struct iphdr *ip_hdr(const struct sk_buff *skb)
-{
-    return (struct iphdr *)(skb->head + ETH_HDR_LEN);
-}
+static inline struct iphdr *ip_hdr(const struct sk_buff *skb) { return (struct iphdr *)(skb->head + ETH_HDR_LEN); }
 
-static inline uint32_t ip_parse(char *addr)
-{
+static inline uint32_t ip_parse(char *addr) {
     uint32_t dst = 0;
-    
+
     if (inet_pton(AF_INET, addr, &dst) != 1) {
         perror("ERR: Parsing inet address failed");
         exit(1);
